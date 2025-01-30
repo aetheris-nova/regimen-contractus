cf30119053679bc79981d28c0908e5dd0c64a4a1
Kieran O'Neill <hello@kieranoneill.com>
2025-01-30 21:02:12 +0000

    build: update soldeer depdendencies on postinstall

--- packages/codex-aetherium/scripts/postinstall.sh
+++ packages/codex-aetherium/scripts/postinstall.sh
@@ -2,8 +2,7 @@
 
 source $(dirname "${BASH_SOURCE[0]}")/set_vars.sh
 
-# Public: Performs post install actions including installing Solidity dependencies and creating the `.env` file if none
-# exist.
+# Public: Performs post install actions including creating the `.env` file if none exist.
 #
 # Examples
 #
@@ -12,15 +11,6 @@ source $(dirname "${BASH_SOURCE[0]}")/set_vars.sh
 # Returns exit code 0.
 function main {
   set_vars
-
-  if [ ! -x "${HOME}/.foundry/bin/forge" ]; then
-    printf "%b foundry not found, installing... \n" "${INFO_PREFIX}"
-    curl -L https://foundry.paradigm.xyz | bash
-    "${HOME}"/.foundry/bin/foundryup
-  fi
-
-  # fetch solidity dependencies
-  "${HOME}"/.foundry/bin/forge soldeer install
 
   # create a .env if none exist
   printf "%b creating .env, if necessary \n" "${INFO_PREFIX}"
